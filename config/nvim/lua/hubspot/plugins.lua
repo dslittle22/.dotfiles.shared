a4b4758b07b0acbc9cf6e96e9b0650aa391a551d
Danny Little <dalittle@hubspot.com>
2026-03-10 11:34:35 -0400

    add eslint

--- config/nvim/lua/hubspot/plugins.lua
+++ config/nvim/lua/hubspot/plugins.lua
@@ -28,6 +28,28 @@ return {
 	},
 
 	{
+		"neovim/nvim-lspconfig",
+		ft = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
+		config = function()
+			vim.lsp.enable("eslint")
+
+			local base_on_attach = vim.lsp.config.eslint.on_attach
+			vim.lsp.config("eslint", {
+				on_attach = function(client, bufnr)
+					if base_on_attach then
+						base_on_attach(client, bufnr)
+					end
+
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						buffer = bufnr,
+						command = "LspEslintFixAll",
+					})
+				end,
+			})
+		end,
+	},
+
+	{
 		"nvim-neotest/neotest",
 		dependencies = {
 			"nvim-neotest/nvim-nio",
